0b587a44b6d19a4ec826418297611aa5b645eafe
Carlie128 <katharina.mayer1@stud.hcw.ac.at>
2026-06-15 09:28:43 +0200

    Home view fixed the issue with the navigation bar not displaying correctly on mobile devices. Adjusted CSS styles and updated the layout to ensure proper responsiveness across different screen sizes.

--- eattheitch/src/components/common/MainNavigation.vue
+++ eattheitch/src/components/common/MainNavigation.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="nav-links">
-    <RouterLink to="/home" class="nav-button">Home</RouterLink>
-    <RouterLink to="/brands" class="nav-button">Brands</RouterLink>
-    <RouterLink to="/trading" class="nav-button">Trading</RouterLink>
-    <RouterLink to="/profile" class="nav-button">Profile</RouterLink>
-  </nav>
+    <nav class="nav-links">
+        <RouterLink to="/home" class="nav-button">Home</RouterLink>
+        <RouterLink to="/brands" class="nav-button">Brands</RouterLink>
+        <RouterLink to="/trading" class="nav-button">Trading</RouterLink>
+        <RouterLink to="/profile" class="nav-button">Profile</RouterLink>
+    </nav>
 </template>
